5edd7d7fe28c8fa70b571e726d7d1c0d4abe1996
sweta <swetakothari541@gmail.com>
2023-01-17 20:11:58 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+       // obj.name = " kunal";
+        obj.setName("kunal");
+        obj.getName();
+    }
   
 }
